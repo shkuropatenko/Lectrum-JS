e62eb94209948165b6e0d386438c4e7fc5449317
shkuropatenko <shkuropatenko.d@gmail.com>
2019-10-28 23:21:31 +0200

    HW

---
+++ lesson-10/1.js
@@ -0,0 +1,80 @@
+/**
+ * Задача 1.
+ *
+ * Напишите имплементацию функции Function.prototype.bind().
+ *
+ * Наша собственная функция bind() должна задавать контекст выполнения целевой функции,
+ * и возвращать новую, привязанную версию целевой функции.
+ *
+ * Функция bind() должна обладать следующими параметрами:
+ * - Первый параметр — это функция, контекст выполнения которой мы хотим привязать;
+ * - Второй параметр — это объект (не массив), в контексте которого нужно вызывать функцию из первого параметра;
+ * - Третий и все остальные параметры — это аргументы для вызова функции из первого параметра.
+ *
+ * Генерировать ошибки если:
+ * - Первый параметр не является типом function;
+ * - Второй параметр не является типом object;
+ * - Второй параметр является массивом.
+ *
+ * Условия:
+ * - Использовать встроенную функцию Function.prototype.bind() запрещено.
+ */
+
+// Решение
+
+// function bind(func, oliver, arg, arg2) {
+//   // this.oliver = oliver.name;
+//   // console.log(oliver.name)
+//   // this.func = oliver;
+//   // console.log(func.apply(this.oliver))
+//   // console.log(func(oliver.name))
+//   // console.log(func.apply(oliver.func()));
+  
+//   if (typeof func !== 'function') {
+//     throw new Error ('First param is not a function!'); 
+//   }
+
+//   if (typeof oliver !== 'object') {
+//     throw new Error ('Second param is not a object!'); 
+//   }
+
+//   if (Array.isArray(oliver)) {
+//     throw new Error ('Second param is a Array!'); 
+//   }
+
+//   return function() {
+//     this.oliver = oliver.name;
+//     return func(arg, arg2, this.oliver)
+//   }
+// }
+
+const bind = (func, context, ...rest) => {
+  
+  if (typeof func !== 'function') {
+    throw new Error ('First param is not a function!'); 
+  }
+
+  if (typeof oliver !== 'object') {
+    throw new Error ('Second param is not a object!'); 
+  }
+
+  if (Array.isArray(oliver)) {
+    throw new Error ('Second param is a Array!'); 
+  }
+
+  return () => func.apply(context, rest);
+};
+
+function getName(greeting, message) {
+  return `${greeting} ${message} ${this.name}.`;
+}
+
+const user = { name: 'Walter', getName };
+const oliver = { name: 'Oliver' };
+
+const boundedGetName = bind(getName, oliver, 'Hello!', 'I am');
+
+console.log(user.getName('Hello!', 'My name is')); // Hello! My name is Walter.
+console.log(boundedGetName()); // Hello! I am Oliver.
+
+// exports.bind = bind;
---
+++ lesson-10/3.js
@@ -0,0 +1,101 @@
+/**
+ * Задача 3.
+ *
+ * Улучшите имплементацию функции calculate() и назовите её calculateAdvanced().
+ * Если на каком-то из вызовов функция-коллбек возбудила ошибку — отловите и сохраните её.
+ *
+ * После отлова ошибки перейдите к выполнению следующего коллбека.
+ *
+ * Улучшенная функция calculateAdvanced() должна возвращать объект с двумя свойствами: `value` и `errors`:
+ * - свойство `value` содержит результат вычисления всех функций из цепочки;
+ * - свойство `errors` содержит массив с объектами, где каждый объект должен обладать следующими свойствами:
+ *     - index — индекс коллбека, на котором ошибка была возбуждена;
+ *     - name — имя ошибки;
+ *     - message — сообщение ошибки.
+ *
+ * Если во время выполнения функции-коллбека возникла ошибка, результат выполнения она не вернёт.
+ *
+ * Поэтому, для продолжения цепочки выполнения
+ * необходимо брать результат последней успешно выполненной функции-коллбека.
+ *
+ * Генерировать ошибки если:
+ * - Любой из аргументов не является функцией (в этой задаче, этой ошибкой нужно именно сломать скрипт, отлавливать её не нужно).
+ */
+
+// Решение
+
+const calculateAdvanced = (...functions) => {
+    const results = [];
+    const errors = [];
+    let newObj = {};
+
+    const init = (index, func, argument) => {
+        try {
+            result.push(index, func, argument)
+
+        } catch (error) {
+            // if (error.name == 'RangeError') {
+            //     throw new ReadError("", error);
+            //   }
+
+            // newObj = {
+            //     index: error.index
+            //     name: error.name
+            //     message: message 
+            // }
+
+            errors.push(error)
+     
+        }
+    };
+
+    functions.forEach((func, index) => {
+        func();
+    });
+
+    return {
+        value: results[results.length - 1],
+        errors,
+    };
+};
+
+const result = calculateAdvanced(
+    () => {},
+    () => {
+        return 7;
+    },
+    () => {},
+    prevResult => {
+        return prevResult + 4;
+    },
+    () => {
+        throw new RangeError('Range is too big.');
+    },
+    prevResult => {
+        return prevResult + 1;
+    },
+    () => {
+        throw new ReferenceError('ID is not defined.');
+    },
+    prevResult => {
+        return prevResult * 5;
+    },
+);
+
+console.log(result);
+
+// Функция вернёт:
+// { value: 60,
+//     errors:
+//      [ { index: 0,
+//          name: 'Error',
+//          message: 'callback at index 0 did not return any value.' },
+//        { index: 2,
+//          name: 'Error',
+//          message: 'callback at index 2 did not return any value.' },
+//        { index: 4, name: 'RangeError', message: 'Range is too big.' },
+//        { index: 6,
+//          name: 'ReferenceError',
+//          message: 'ID is not defined.' } ] }
+
+// exports.calculateAdvanced = calculateAdvanced;
