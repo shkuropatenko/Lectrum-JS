7c63de27943768a75ec73bea2a6d021861443682
shkuropatenko <shkuropatenko.d@gmail.com>
2019-11-15 17:48:28 +0200

    add HW

---
+++ lesson-17/1.js
@@ -0,0 +1,52 @@
+/**
+ * Задача 1.
+ *
+ * Напишите функцию postpone, которая выводит в консоль счетчик с задержкой.
+ *
+ * Функция принимает 3 параметра:
+ * - Первый параметр `start` — число, c которого начнется отсчет;
+ * - Второй параметр `end` — верхний порог, до которого будет идти отсчет;
+ * - Третий параметр `delay` — это время в `мс` между выводами.
+ *
+ * Условия:
+ * - Функция принимает три параметра;
+ * - Функция содержит валидацию входных параметров на тип number;
+ * - Обязательно использование таймера setTimeout и цикла for;
+ * - Функция должна уметь считать в обе стороны.
+ */
+
+// Решение
+function postpone(start, end, delay) {
+
+    let arr = arguments;
+    for (let value of arr) {
+        if (typeof value !== 'number') {
+            throw new Error ('Should be number!');
+        }
+    }
+    
+    let timerId = setTimeout(function tick() {
+        if (start < end) {
+            for(let i = start; i <= end; i++) {
+                console.log(i);
+                clearTimeout(timerId)
+            }
+        } else {
+            for(start; start >= 1; start--) {
+               console.log(start)
+            }
+        }
+    }, delay);
+}
+
+
+postpone(1, 3, 1000);
+// 1
+// 2
+// 3
+postpone(3, 1, 1000);
+// 3
+// 2
+// 1
+
+// exports.postpone = postpone;
