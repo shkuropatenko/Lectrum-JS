a95d3d6dd82bdf3614a61dc54296f8dfc7185314
Markupus <dmitriy@markupus.tech>
2019-11-29 17:55:54 +0200

    add HW

---
+++ lesson-20/1.js
@@ -0,0 +1,65 @@
+/*
+Задача 1
+
+Создайте класс `Customers` который умеет работать с механизмом `for of`.
+
+1. Класс `Customers` содержит метод `add` который принимает объект в качестве параметра. У этого объекта есть обязательное поле `name` и необязательное поле `verified`.
+2. Класс `Customers` при переборе с помощью `for of` должен учитывать только объекты у которых был установлен флаг `verified: true`.
+
+**Обратите внимание**:
+1. Использование генераторов **запрещено**.
+2. Использование посторонних библиотек **запрещено**
+3. У класса `Customers` **должен** быть метод `Symbol.iterator`
+*/
+
+
+// Решение
+class Customers {
+	constructor() {
+		this.arr = new Array();
+    }
+
+    add(param) {
+        this.arr.push(param);
+    }
+
+    [Symbol.iterator]() {
+        let i = 0;
+        const result = this.arr.filter(word => word.verified === true);
+
+        return {
+            next: () => {
+                const done = i >= result.length;
+                const value = !done ? result[i++] : undefined;
+
+                return {
+                    value,
+                    done
+                }
+            },
+        };
+    }
+}
+
+// Пример использования
+const customers = new Customers();
+customers.add({name: 'Alex'});
+customers.add({name: 'Victor'});
+customers.add({name: 'Marcus'});
+customers.add({name: 'Andrii', verified: true});
+customers.add({name: 'Marco', verified: true});
+customers.add({name: 'Oliver'});
+customers.add({name: 'Lisa', verified: true});
+customers.add({name: 'John'});
+customers.add({name: 'Ivan', verified: true});
+
+for (const customer of customers) {
+    console.log(customer);
+}
+
+
+// В консоли будет
+// { name: 'Andrii', verified: true }
+// { name: 'Marco', verified: true }
+// { name: 'Lisa', verified: true }
+// { name: 'Ivan', verified: true }
---
+++ lesson-20/2.js
@@ -0,0 +1,54 @@
+/** 
+# Задача 2
+
+Улучшите класс `Customers` добавив функцию генератор.
+
+**Обратите внимание**:
+
+1. Класс `Customers` после этого должен работать **идентично** предыдущей задаче.
+*/
+
+// Решение
+class Customers {
+    constructor() {
+         this.arr = new Array();
+    }
+
+    add(param) {
+        this.arr.push(param)
+    }
+
+    *[Symbol.iterator]() {
+       for (const item of this.arr) {
+            if(item.verified === true) {
+                yield item;
+            }
+        }
+    }
+}
+
+// Пример использования:
+const customers = new Customers();
+customers.add({name: 'Alex'});
+customers.add({name: 'Victor'});
+customers.add({name: 'Marcus'});
+customers.add({name: 'Andrii', verified: true});
+customers.add({name: 'Marco', verified: true});
+customers.add({name: 'Oliver'});
+customers.add({name: 'Lisa', verified: true});
+customers.add({name: 'John'});
+customers.add({name: 'Ivan', verified: true});
+
+for (const customer of customers) {
+    console.log(customer);
+}
+
+// const iter = customers[Symbol.iterator]();
+// console.log(iter.next());
+
+
+// В консоли будет
+// { name: 'Andrii', verified: true }
+// { name: 'Marco', verified: true }
+// { name: 'Lisa', verified: true }
+// { name: 'Ivan', verified: true }
